8409c5c559f4c674f44408a7c6954ca846c30d4b
Robert Verst <robert@verst.eu>
2022-06-12 11:34:21 +0200

    Add info, fix typo
    Signed-off-by: Robert Verst <robert@verst.eu>

--- themes/bb2018/assets/script/utils.js
+++ themes/bb2018/assets/script/utils.js
@@ -1,10 +1,13 @@
 export function unveilMail(rec) {
-    const c = 'HnB7@E3k1YABF8-E1sL1YbaaL.81';
+    const c = 'HnB7@E3k1YABF8-E1sL1YhaaL.81';
     const k = 'j2JIAOE6PsGkh1q7r9WB3MRzvoTn4dSUD0NXCHlLBfgcYtwe5mZxiauFQ8VyKp';
     let s = c.length;
     let l = '';
     for (let i = 0; i < c.length; i++) {
-        if (k.indexOf(c.charAt(i)) === -1) {
+				if (c.charAt(i) === 'h') {
+				    l += c.charAt(i)
+				}		
+				else if (k.indexOf(c.charAt(i)) === -1) {
             let ltr = c.charAt(i);
             l += (ltr)
         } else {
